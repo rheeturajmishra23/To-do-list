7bac6a8196c764e07bec51dce401684f209d2281
“Rheeturaj <rheeturaj.mishra1@gmail.com>
2023-12-13 08:40:11 +0530

    complete project

---
+++ app.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const app = express()
+const tasks = require('./routes/tasks')
+const connectDB = require('./db/connect')
+require('dotenv').config()
+
+// middleware
+app.use(express.static("./public"))
+app.use(express.json())
+
+//routes
+app.get('/',(req,res)=>{
+    res.send('Task Manager')
+})
+app.use('/api/v1/tasks',tasks)
+
+const port = 3000;
+
+const start = async () => {
+    try {
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port, console.log(`server listening at ${port}..`))
+    } catch (error) {
+        console.log(error)
+    }
+}
+start()
